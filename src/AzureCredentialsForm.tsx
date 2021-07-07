3c3e350a8d3586aea5468480ae93efc9ad61ad53
joshhunt <josh@trtr.co>
2021-07-07 18:11:11 +0100

    Initial commit of scaffold

---
+++ src/AzureCredentialsForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export interface AzureCredentialsFormProps {}
+
+export const AzureCredentialsForm: React.FC<AzureCredentialsFormProps> = () => {
+  return <div>AzureCredentialsForm component</div>;
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { AzureCredentialsForm, AzureCredentialsFormProps } from './AzureCredentialsForm';
