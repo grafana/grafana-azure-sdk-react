2b5455e5b04564e2bb8d7f1fba9359fed01dfd21
Sergey Kostrukov <sekost@microsoft.com>
2024-02-26 16:23:25 -0800

    Clouds from Grafana backend

--- src/clouds.ts
+++ src/clouds.ts
@@ -1,3 +1,9 @@
+import { AzureSettings, config } from '@grafana/runtime';
+
+interface AzureSettings2 extends AzureSettings {
+  clouds?: AzureCloudInfo[];
+}
+
 export interface AzureCloudInfo {
   name: string;
   displayName: string;
@@ -16,5 +22,12 @@ const predefinedClouds: AzureCloudInfo[] = [
 ];
 
 export function getAzureClouds(): AzureCloudInfo[] {
+  const settingsEx = (config.azure as unknown as AzureSettings2);
+
+  // Return list of clouds from Grafana configuration if they are provided
+  if (Array.isArray(settingsEx.clouds)) {
+    return settingsEx.clouds;
+  }
+
   return predefinedClouds;
 }
