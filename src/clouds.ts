d0640f3b92a0be47f67067aa92e02eb83c36b108
Andreas Christou <andreas.christou@grafana.com>
2024-03-14 14:38:45 +0000

    Add nightly dependency and remove as

--- src/clouds.ts
+++ src/clouds.ts
@@ -1,10 +1,4 @@
-import { AzureSettings, config } from '@grafana/runtime';
-
-// TODO: Remove when the list of clouds added directly to @grafana/runtime
-//  https://github.com/grafana/grafana/pull/84039
-interface AzureSettings2 extends AzureSettings {
-  clouds?: AzureCloudInfo[];
-}
+import { config } from '@grafana/runtime';
 
 export interface AzureCloudInfo {
   name: string;
@@ -13,18 +7,21 @@ export interface AzureCloudInfo {
 
 const predefinedClouds: AzureCloudInfo[] = [
   {
-    name: 'AzureCloud', displayName: 'Azure'
+    name: 'AzureCloud',
+    displayName: 'Azure',
   },
   {
-    name: 'AzureChinaCloud', displayName: 'Azure China'
+    name: 'AzureChinaCloud',
+    displayName: 'Azure China',
   },
   {
-    name: 'AzureUSGovernment', displayName: 'Azure US Government'
-  }
+    name: 'AzureUSGovernment',
+    displayName: 'Azure US Government',
+  },
 ];
 
 export function getAzureClouds(): AzureCloudInfo[] {
-  const settingsEx = (config.azure as unknown as AzureSettings2);
+  const settingsEx = config.azure;
 
   // Return list of clouds from Grafana configuration if they are provided
   if (Array.isArray(settingsEx.clouds)) {
