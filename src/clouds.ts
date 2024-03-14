0a30fa6afdeb756a167e2662ba7a6154e9ad0635
Sergey Kostrukov <sekost@microsoft.com>
2024-03-14 06:10:17 -0700

    List of Azure clouds from Grafana backend (#128)
    * More ignores
    
    * Clouds from Grafana backend
    
    * Add TODO for AzureSettings2

--- src/clouds.ts
+++ src/clouds.ts
@@ -1,3 +1,11 @@
+import { AzureSettings, config } from '@grafana/runtime';
+
+// TODO: Remove when the list of clouds added directly to @grafana/runtime
+//  https://github.com/grafana/grafana/pull/84039
+interface AzureSettings2 extends AzureSettings {
+  clouds?: AzureCloudInfo[];
+}
+
 export interface AzureCloudInfo {
   name: string;
   displayName: string;
@@ -16,5 +24,12 @@ const predefinedClouds: AzureCloudInfo[] = [
 ];
 
 export function getAzureClouds(): AzureCloudInfo[] {
+  const settingsEx = (config.azure as unknown as AzureSettings2);
+
+  // Return list of clouds from Grafana configuration if they are provided
+  if (Array.isArray(settingsEx.clouds)) {
+    return settingsEx.clouds;
+  }
+
   return predefinedClouds;
 }
