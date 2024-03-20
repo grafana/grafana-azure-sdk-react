c81218a7e49aa7d98560dca8cdb676bb1a3f9110
Andreas Christou <andreas.christou@grafana.com>
2024-03-20 20:20:11 +0000

    Update clouds and bump version (#133)
    * Add nightly dependency and remove as
    
    * Bump version
    
    * Remove unnecessary type and use import

--- src/clouds.ts
+++ src/clouds.ts
@@ -1,30 +1,22 @@
-import { AzureSettings, config } from '@grafana/runtime';
-
-// TODO: Remove when the list of clouds added directly to @grafana/runtime
-//  https://github.com/grafana/grafana/pull/84039
-interface AzureSettings2 extends AzureSettings {
-  clouds?: AzureCloudInfo[];
-}
-
-export interface AzureCloudInfo {
-  name: string;
-  displayName: string;
-}
+import { AzureCloudInfo, config } from '@grafana/runtime';
 
 const predefinedClouds: AzureCloudInfo[] = [
   {
-    name: 'AzureCloud', displayName: 'Azure'
+    name: 'AzureCloud',
+    displayName: 'Azure',
   },
   {
-    name: 'AzureChinaCloud', displayName: 'Azure China'
+    name: 'AzureChinaCloud',
+    displayName: 'Azure China',
   },
   {
-    name: 'AzureUSGovernment', displayName: 'Azure US Government'
-  }
+    name: 'AzureUSGovernment',
+    displayName: 'Azure US Government',
+  },
 ];
 
 export function getAzureClouds(): AzureCloudInfo[] {
-  const settingsEx = (config.azure as unknown as AzureSettings2);
+  const settingsEx = config.azure;
 
   // Return list of clouds from Grafana configuration if they are provided
   if (Array.isArray(settingsEx.clouds)) {
