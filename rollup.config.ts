a618d6c089ab450d6e896cc291f56749b35018aa
Andreas Christou <andreas.christou@grafana.com>
2025-10-06 17:33:48 +0100

    Minor workflow improvements and rollup fix

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import { externals } from 'rollup-plugin-node-externals';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 const packagePath = './package.json';
 
